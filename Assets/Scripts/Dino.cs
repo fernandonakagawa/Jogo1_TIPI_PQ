f21521524a94fb21ba30676e06d9ffb18aa9af9d
Fernando Henrique Yoshiaki Nakagawa <fernando.nakagawa@solmarista.org.br>
2021-08-17 15:16:52 -0300

    Respawn do personagem

--- Assets/Scripts/Dino.cs
+++ Assets/Scripts/Dino.cs
@@ -7,16 +7,13 @@ public class Dino : MonoBehaviour
     public float velocidade;
     public float tempoPulo;
     private float tempoPulado;
-    // Start is called before the first frame update
+    private Vector3 posicaoInicial;
     void Start()
     {
-
+        posicaoInicial = this.transform.position;
     }
-
-    // Update is called once per frame
     void Update()
     {
-
         if (Input.GetKey(KeyCode.D))
         {
             Vector3 pos = this.transform.position;
@@ -38,8 +35,18 @@ public class Dino : MonoBehaviour
             rb.AddForce(forca, ForceMode2D.Impulse);
             tempoPulado = tempoPulo;
         }
-        //Debug.Log(Time.deltaTime);
         tempoPulado -= Time.deltaTime;
+        if(this.transform.position.y < -14f)
+        {
+            this.transform.position = posicaoInicial;
+        } 
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.tag == "cogumelo_ruim")
+        {
+            this.transform.position = posicaoInicial;
+        }
     }
 
 }

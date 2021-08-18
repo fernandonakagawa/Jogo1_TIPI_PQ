b5f02fac8feb62259f1b6ca186faca001912fbac
Fernando Henrique Yoshiaki Nakagawa <fernando.nakagawa@solmarista.org.br>
2021-08-18 14:21:00 -0300

    Personagem andando e pulando

--- Assets/Scripts/Dino.cs
+++ Assets/Scripts/Dino.cs
@@ -8,14 +8,19 @@ public class Dino : MonoBehaviour
     public float tempoPulo;
     private float tempoPulado;
     private Vector3 posicaoInicial;
+    private Animator anim;
+    private SpriteRenderer sprite;
     void Start()
     {
         posicaoInicial = this.transform.position;
+        anim = this.GetComponent<Animator>();
+        sprite = this.GetComponent<SpriteRenderer>();
     }
     void Update()
     {
         if (Input.GetKey(KeyCode.D))
         {
+            sprite.flipX = false;
             Vector3 pos = this.transform.position;
             //pos.x = pos.x + velocidade;
             pos.x += velocidade;
@@ -23,6 +28,7 @@ public class Dino : MonoBehaviour
         }
         if (Input.GetKey(KeyCode.A))
         {
+            sprite.flipX = true;
             Vector3 pos = this.transform.position;
             //pos.x = pos.x + velocidade;
             pos.x -= velocidade;
@@ -34,18 +40,32 @@ public class Dino : MonoBehaviour
             Vector2 forca = new Vector2(0f, 10f);
             rb.AddForce(forca, ForceMode2D.Impulse);
             tempoPulado = tempoPulo;
+            anim.SetBool("estaPulando", true);
         }
         tempoPulado -= Time.deltaTime;
         if(this.transform.position.y < -14f)
         {
             this.transform.position = posicaoInicial;
-        } 
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A))
+        {
+            anim.SetBool("estaAndando", true);
+        }
+        else
+        {
+            anim.SetBool("estaAndando", false);
+        }
+
     }
     void OnCollisionEnter2D(Collision2D col)
     {
         if(col.gameObject.tag == "cogumelo_ruim")
         {
             this.transform.position = posicaoInicial;
+        }
+        if(col.gameObject.tag == "chao")
+        {
+            anim.SetBool("estaPulando", false);
         }
     }
 
